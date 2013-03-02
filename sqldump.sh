eaa72898307dcacdaf42134b879396b057ec6ee5
Dawid Pych <root@ks3309521.kimsufi.com>
2013-03-02 21:48:24 +0100

    2013.03.02 Kopia bazy danych

--- sqldump.sh
+++ sqldump.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 user="c7_wineacademy"
 pass="Conan252N"
-base="c7_sandbox"
+base="c7_wineacademy"
+email="info@wineacademy.pl"
+emailadmin="dawidpych@gmail.com"
+temat="Serwer - Kopia bazy danych WineAcademy"
+tresc="/tmp/emailmessage.txt"
 rm sql/*.sql
 mysqldump -l -u$user -p$pass $base > sql/$(date +"%Y%m%d").sql
 git add -A
 git commit -m $(date +"%Y")"."$(date +"%m")"."$(date +"%d")" Kopia bazy danych"
 git push origin master
+chmod 777 -R *
+echo "Wiadomość wygenerowana przez serwer. Nie odpisuje" > $tresc
+echo "plik do pobrania http://wineacademy.pl/backup/sql/"$(date +"%Y%m%d")".sql" >> $tresc
+/bin/mail -s "$temat" "$email" < $tresc
