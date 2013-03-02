8f30ff393f0195bcb1630e63f8b4a485a9de8713
Dawid Pych <root@ks3309521.kimsufi.com>
2013-03-02 20:14:51 +0100

    2013...03...02. Kopia bazy danych

--- sqldump.sh
+++ sqldump.sh
@@ -2,7 +2,8 @@
 user="c7_wineacademy"
 pass="Conan252N"
 base="c7_sandbox"
+rm sql/*.sql
 mysqldump -l -u$user -p$pass $base > sql/$(date +"%Y%m%d").sql
 git add -A
-git commit -m $(date +"%Y%m%d")." Kopia bazy danych"
+git commit -m $(date +"%Y").".".$(date +"%m").".".$(date +"%d")." Kopia bazy danych"
 git push origin master
